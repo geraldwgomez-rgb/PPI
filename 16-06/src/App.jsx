e1d02b866bb7507b18c1a8ec3b53fb6aac637e07
gerald-77 <geraldwgomez@gmail.com>
2026-07-19 23:35:54 +0000

    Login con Supabase integrado - Sistema SMC

--- 16-06/src/App.jsx
+++ 16-06/src/App.jsx
@@ -1,25 +1,38 @@
+import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { supabase } from './lib/supabaseClient'
 
 import Layout from './components/layout/Layout'
-
+import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import UsuariosPage from './pages/UsuariosPage'
 import GastosPage from './pages/GastosPage'
 import IngresosPage from './pages/IngresosPage'
 import PresupuestosPage from './pages/PresupuestosPage'
-import LoginPage from './pages/LoginPage'
-import RegistroPage from './pages/RegistroPage'
 
 import './index.css'
 
 function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    return () => listener.subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return <LoginPage onLogin={setSession} />
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/registro" element={<RegistroPage />} />
-
-        <Route element={<Layout />}>
+        <Route element={<Layout session={session} />}>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/usuarios" element={<UsuariosPage />} />
           <Route path="/gastos" element={<GastosPage />} />
